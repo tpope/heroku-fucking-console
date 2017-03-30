283c934f2cda2d295fc46d961d4aff4cd13ac6d9
Tim Pope <code@tpope.net>
2017-03-30 18:05:43 -0400

    Fuck this subversive attempt to force porting to Node

--- lib/heroku/command/fucking.rb
+++ lib/heroku/command/fucking.rb
@@ -10,7 +10,7 @@ class Heroku::Command::Fucking < Heroku::Command::Run
   # The exact command to run is chosen based on the CONSOLE config var or
   # heuristically based on the buildpack.
   def console
-    console = api.get_config_vars(app).body.fetch('CONSOLE') do
+    console = config_vars.fetch('CONSOLE') do
       case api.get_app(app).body['buildpack_provided_description']
       when 'Ruby' then 'console'
       when 'Scala' then 'sbt console'
@@ -31,5 +31,16 @@ class Heroku::Command::Fucking < Heroku::Command::Run
     run_attached((["rake"] + args).join(' '))
   end
   alias_command 'rake', 'fucking:rake'
+
+  private
+  def config_vars
+    begin
+      require 'platform-api'
+      @api ||= PlatformAPI.connect(Heroku::Auth.password)
+      @api.config_var.info_for_app(app)
+    rescue LoadError
+      api.get_config_vars(app).body
+    end
+  end
 
 end
